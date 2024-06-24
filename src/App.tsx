e14e5fbd757f73c7231a61ab8489355b73767521
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2024-06-24 16:26:51 -0500

    active id en reduces

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
+import ActivityList from "./components/ActivityList"
 
 function App() {
 
@@ -25,6 +26,13 @@ function App() {
             dispatch={dispatch}
           />
         </div>
+      </section>
+
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList
+          activities={state.activities}
+          dispatch={dispatch}
+        />
       </section>
     </>
   )
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,16 +1,17 @@
 import { Activity } from "../types"
 
-export type ActivityActions = {
-	type: "save-activity"
-	payload: { newActivity: Activity }
-}
+export type ActivityActions =
+	| { type: "save-activity"; payload: { newActivity: Activity } }
+	| { type: "set-activeId"; payload: { id: Activity["id"] } }
 
 type ActivityState = {
 	activities: Activity[]
+	activeId: Activity["id"]
 }
 
 export const initialState: ActivityState = {
 	activities: [],
+	activeId: "",
 }
 
 export const activityReducer = (
@@ -24,6 +25,14 @@ export const activityReducer = (
 		return {
 			...state,
 			activities: [...state.activities, action.payload.newActivity],
+		}
+	}
+
+	if (action.type === "set-activeId") {
+		console.log("algo")
+		return {
+			...state,
+			activeId: action.payload.id,
 		}
 	}
 
