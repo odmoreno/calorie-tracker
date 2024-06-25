577a09083ab3c8afdb726f63eb3c29b4198b9527
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2024-06-25 16:18:01 -0500

    Reducer set activity

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
 
           <Form
             dispatch={dispatch}
+            state={state}
           />
         </div>
       </section>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,31 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid'
 import { categories } from "../data/categories"
 import type { Activity } from "../types"
-import { ActivityActions } from "../reducers/activity-reducer"
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
 
 type FormProps = {
-  dispatch: Dispatch<ActivityActions>
+  dispatch: Dispatch<ActivityActions>,
+  state: ActivityState
 }
 
-export default function Form({ dispatch }: FormProps) {
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: '',
+  calories: 0
+}
 
-  const initialState: Activity = {
-    id: uuidv4(),
-    category: 1,
-    name: '',
-    calories: 0
-  }
+export default function Form({ dispatch, state }: FormProps) {
 
   const [activity, setActivity] = useState<Activity>(initialState)
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+      setActivity(selectedActivity)
+    }
+  }, [state.activeId, state.activities])
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category', 'calories'].includes(e.target.id)
