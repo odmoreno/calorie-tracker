15d093bab13b224a305e3800d8e7613182554495
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2024-07-01 15:02:15 -0500

    contador calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -7,12 +7,14 @@ type CalorieTrackerProps = {
 }
 
 export default function CalorieTracker({ activities }: CalorieTrackerProps) {
-
   const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
 
   return (
     <>
       <h2 className=" text-4xl font-black text-white text-center"> Resumen de calorias</h2>
+      <div className=" flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+
+      </div>
       <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
 
         {caloriesConsumed}
